63baa323fa90df4467cf14be7b08fdf7d74c1cbe
Andreas Fritzler <andreas.fritzler@proton.me>
2022-08-25 10:58:21 +0200

    Bump golang to 1.19

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Andreas Fritzler <andreas.fritzler@gmail.com>
+// Copyright © 2018 Andreas Fritzler
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
